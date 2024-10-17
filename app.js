41a694466b45620fdb688e5f306afd833d2bb661
yoxiaoxiao <xiaoxiaofu2866@gmail.com>
2024-10-17 16:24:40 -0700

    finished

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.get("/status", (req, res) => {
+    res.send("The server is alive!");
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 });
 
-app.get("/status", (req, res) => {
-    res.send("The server is alive!");
-});
